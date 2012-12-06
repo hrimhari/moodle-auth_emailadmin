a0a681e5454142e5cca281d454b5234de3253deb
root <felipe@carassonet.org>
2012-12-06 01:04:35 -0500

    - Updated version requirements to publish plugin to Moodle site.

--- version.php
+++ version.php
@@ -27,5 +27,5 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2012120300;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2012061700;        // Requires this Moodle version
+$plugin->requires  = 2012062500;        // Requires this Moodle version
 $plugin->component = 'auth_emailadmin';      // Full name of the plugin (used for diagnostics)
