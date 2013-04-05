60a6400aae811320d003a6206226f7e213e7b360
hrimhari <felipe@carassonet.org>
2013-04-04 22:19:51 -0300

    Update version.php
    - Added "maturity" and "release" to solve Moodle validation warnings.

--- version.php
+++ version.php
@@ -29,3 +29,5 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version   = 2012120300;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012061700;        // Requires this Moodle version
 $plugin->component = 'auth_emailadmin';      // Full name of the plugin (used for diagnostics)
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '1.0 (2012120300)';
