9966fb72e976ae7358db21ba381c3027156a7369
Felipe Carasso <felipe@carassonet.org>
2013-04-05 08:25:47 -0300

    Update version.php
    - Updated requires to official 2.3 release.

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2012120300;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2012061700;        // Requires this Moodle version
+$plugin->requires  = 2012062500;        // Requires this Moodle version
 $plugin->component = 'auth_emailadmin';      // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_RC;
 $plugin->release = '1.0 (2012120300)';
