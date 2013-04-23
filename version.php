747a01e30631f22f1bc828fd5382aa274db2d54e
Didier Raboud <didier.raboud@liip.ch>
2013-04-23 17:20:27 +0200

    Take the requirements down to try pushing that to Totara

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2012120302;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2012062500;        // Requires this Moodle version
+$plugin->requires  = 2011120509;        // Requires this Moodle version
 $plugin->component = 'auth_emailadmin';      // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_RC;
 $plugin->release = '1.0 (2012120302)';
