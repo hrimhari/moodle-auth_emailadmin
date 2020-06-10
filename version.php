ca756b13a9706d9604255b4387302aba2cb95cc0
Felipe Carasso <felipe@carassonet.org>
2020-06-10 00:35:52 -0400

    chore: remove functions from db upgrade deprecated on Moodle 3.9

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2019072300;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2015051101;        // Requires this Moodle version.
+$plugin->version   = 2030061000;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2017111300;        // Requires this Moodle version.
 $plugin->component = 'auth_emailadmin';      // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.4.4';
+$plugin->release = '1.4.5';
